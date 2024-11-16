d56171a849a42b5e6432f4a0b14766a5bb6dbf78
Swara Sameer Pawanekar <156062610+swara063@users.noreply.github.com>
2024-11-17 04:26:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from utils.jobrecommend import jobrecommend_ui
+from utils.markettrend import markettrend_ui
+from utils.skillgap import skillgap_ui
+from utils.airesume import airesume_ui
+from utils.chatbot import chatbot_ui
+
+# App Title
+st.title("Career Assistance Tool")
+
+# Sidebar navigation
+st.sidebar.title("Navigation")
+options = {
+    "Best Job Fit Applications": jobrecommend_ui,
+    "Market Trend Analysis": markettrend_ui,
+    "Skill Gap Identification": skillgap_ui,
+    "Resume Parsing": airesume_ui,
+    "Career Advice Chatbot": chatbot_ui,
+}
+
+# Function selection and display
+selection = st.sidebar.radio("Choose a Functionality", list(options.keys()))
+options[selection]()  # Call the selected functionality's UI function
