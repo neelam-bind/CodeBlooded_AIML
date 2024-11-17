f86718a19488a245bf34c146fa02b09dcdf89ade
Swara Sameer Pawanekar <156062610+swara063@users.noreply.github.com>
2024-11-17 05:38:37 +0530

    Update skillgap.py

--- utils/skillgap.py
+++ utils/skillgap.py
@@ -68,9 +68,51 @@ def recommend_jobs_from_resume(resume_pdf_path, top_n=5):
 def suggest_learning_resources(skill_gaps):
     """Suggests learning resources for the given skill gaps."""
     resources = {
-        'python': [{'name': 'Python for Data Science', 'url': 'https://www.coursera.org/specializations/python-data-science'}],
-        'java': [{'name': 'Java Programming (Udemy)', 'url': 'https://www.udemy.com/course/java-programming-complete-beginner-to-advanced/'}],
-        'machine learning': [{'name': 'Machine Learning by Andrew Ng', 'url': 'https://www.coursera.org/learn/machine-learning'}],
+        'python': [
+            {'name': 'Python for Data Science (Coursera)', 'url': 'https://www.coursera.org/specializations/python-data-science'},
+            {'name': 'Python Crash Course (Book)', 'url': 'https://nostarch.com/pythoncrashcourse2e'}
+        ],
+        'java': [
+            {'name': 'Java Programming (Udemy)', 'url': 'https://www.udemy.com/course/java-programming-complete-beginner-to-advanced/'},
+            {'name': 'Effective Java (Book)', 'url': 'https://www.pearson.com/store/p/effective-java/P100000535370'}
+        ],
+        'machine learning': [
+            {'name': 'Machine Learning by Andrew Ng (Coursera)', 'url': 'https://www.coursera.org/learn/machine-learning'},
+            {'name': 'Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow (Book)', 'url': 'https://www.oreilly.com/library/view/hands-on-machine-learning/9781492032632/'}
+        ],
+        'deep learning': [
+            {'name': 'Deep Learning Specialization (Coursera)', 'url': 'https://www.coursera.org/specializations/deep-learning'},
+            {'name': 'Deep Learning (Book)', 'url': 'https://www.deeplearningbook.org/'}
+        ],
+        'tensorflow': [
+            {'name': 'TensorFlow for Beginners (Udemy)', 'url': 'https://www.udemy.com/course/learn-tensorflow/'},
+            {'name': 'Hands-On Machine Learning with TensorFlow (Book)', 'url': 'https://www.oreilly.com/library/view/hands-on-machine-learning/9781491978499/'}
+        ],
+        'aws': [
+            {'name': 'AWS Certified Solutions Architect - Associate (Udemy)', 'url': 'https://www.udemy.com/course/aws-certified-solutions-architect-associate/'},
+            {'name': 'AWS Certified Solutions Architect – Official Study Guide (Book)', 'url': 'https://www.amazon.com/AWS-Certified-Solutions-Architect-Associate/dp/1119504212'}
+        ],
+        'sql': [
+            {'name': 'SQL for Data Science (Coursera)', 'url': 'https://www.coursera.org/learn/sql-for-data-science'},
+            {'name': 'Learning SQL (Book)', 'url': 'https://www.oreilly.com/library/view/learning-sql/9781449319267/'}
+        ],
+        'docker': [
+            {'name': 'Docker for Beginners (Udemy)', 'url': 'https://www.udemy.com/course/docker-tutorial-for-beginners/'},
+            {'name': 'Docker Deep Dive (Book)', 'url': 'https://www.amazon.com/Docker-Deep-Dive-Nigel-Poulton/dp/1521822807'}
+        ],
+        # Add more skills and resources here
+        'communication': [
+            {'name': 'Communication Skills for Professionals (Coursera)', 'url': 'https://www.coursera.org/learn/wharton-communication-skills'},
+            {'name': 'How to Win Friends and Influence People (Book)', 'url': 'https://www.amazon.com/How-Win-Friends-Influence-People/dp/0671027034'}
+        ],
+        'leadership': [
+            {'name': 'Leadership Skills for Beginners (Udemy)', 'url': 'https://www.udemy.com/course/leadership-skills-for-beginners/'},
+            {'name': 'Leaders Eat Last (Book)', 'url': 'https://www.amazon.com/Leaders-Eat-Last-Together-Business/dp/1591848016'}
+        ],
+        'project management': [
+            {'name': 'Project Management for Beginners (Udemy)', 'url': 'https://www.udemy.com/course/project-management-for-beginners/'},
+            {'name': 'Project Management: A Systems Approach to Planning, Scheduling, and Controlling (Book)', 'url': 'https://www.amazon.com/Project-Management-Systems-Planning-Scheduling/dp/1119165350'}
+        ]
     }
     suggestions = {}
     for skill in skill_gaps:
