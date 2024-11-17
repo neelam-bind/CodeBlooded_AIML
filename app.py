8c20847b812df493503c4305945cc3b72efd76cf
Swara Sameer Pawanekar <156062610+swara063@users.noreply.github.com>
2024-11-17 09:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from utils.jobrecommend import jobrecommend_ui
 from utils.markettrend import markettrend_ui
 from utils.skillgap import skillgap_ui
 from utils.airesume import airesume_ui
-#from utils.chatbot import chatbot_ui
+from utils.chatbot import chatbot_ui
 
 # App Title
 st.title("HireScope")
@@ -17,7 +17,7 @@ options = {
     "Market Trend Analysis": markettrend_ui,
     "Skill Gap Identification": skillgap_ui,
     "Resume Parsing": airesume_ui,
-    #"Career Advice Chatbot": chatbot_ui,
+    "Career Advice Chatbot": chatbot_ui,
 }
 
 # Function selection and display
